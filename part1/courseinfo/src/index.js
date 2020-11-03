134888e1ed20894a0c01414e9e7978ea9ae8cc7c
jakziema <ziemannjakub@gmail.com>
2020-11-03 19:23:58 +0000

    1c

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ReactDOM from 'react-dom';
+
+
 
 
 const Header = (props) => {
@@ -8,6 +10,23 @@ const Header = (props) => {
     
     <div>
     <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Hello = ({name, age}) => {
+  const bornYear = () =>  new Date().getFullYear() - age
+    
+  
+
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>
+        So you were probably born in {bornYear()}
+      </p>
     </div>
   )
 }
@@ -38,7 +57,30 @@ const Total = (props) => {
   )
 }
 
+const Display = ({counter}) => {
+  return (
+  <div>{counter}</div>
+  )
+}
+
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick = {handleClick}>
+      {text}
+    </button>
+  )
+}
+
 const App = () => {
+
+  const [counter, setCounter] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+ 
+
   const course = 'Half Stack application development'
   const parts = [
     {
@@ -60,11 +102,18 @@ const App = () => {
       <Header course = {course}></Header>
       <Course parts = {parts} ></Course>
       <Total parts = {parts}></Total>
+      <Hello name = "Jakub" age = "26"></Hello>
+      <Display counter = {counter}></Display>
+      <Button handleClick = {increaseByOne} text = 'plus' ></Button>
+      <Button handleClick = {setToZero} text = 'zero' ></Button>
     </div>
-    
-      
     
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
+
+
